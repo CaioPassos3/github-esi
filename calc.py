b0d84deb84b8b5375a5014f655c6a17fc2ad2fac
Caio Passos <caioplima2@gmail.com>
2025-05-14 18:44:19 -0300

    Ajuste em sum() com docstring

--- calc.py
+++ calc.py
@@ -1,8 +1,9 @@
 def sub(x, y):
     return x - y
 
-  def sum(x, y):
-      return x + y
+def sum(x, y):
+    """Soma dois números inteiros."""  # abc
+    return x + y
 
   def div(x, y):
       return x / y if y != 0 else None
